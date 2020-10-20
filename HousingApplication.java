5b88156271c8aeaa5710d0ee71090ab6ecaefa64
michellemccth3 <70456429+michellemccth3@users.noreply.github.com>
2020-10-20 13:41:52 -0400

    Add files via upload

---
+++ HousingApplication.java
@@ -0,0 +1,19 @@
+package housingapplication;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.*;
+
+public class HousingApplication extends Application{
+   public void init(){
+   
+   }
+   
+   public void start(Stage stage){
+   
+   {
+   
+   public void stop(){
+   
+   }
+}
