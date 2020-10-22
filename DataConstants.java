c21117d7d613f1a30f0e69954306f9156f74adb1
jmccutcheon16 <49525814+jmccutcheon16@users.noreply.github.com>
2020-10-22 14:40:17 -0400

    Created DATACONSTANTS

---
+++ DataConstants.java
@@ -0,0 +1,15 @@
+
+public abstract class DataConstants {
+
+	protected static final String FILE = "src/json_User.json";
+	protected static final String USER_FIRST_NAME = "";
+	protected static final String USER_LAST_NAME = "";
+	protected static final String USER_EMAIL = "";
+	protected static final String USER_PASSWORD = "";
+	protected static final int USER_PHONE_NUM = "";
+	protected static final boolean REGISTERED = "";
+	protected static final boolean TENANT = "";
+	protected static final boolean OWNER = "";
+	protected static final int RATING = "";
+	
+}
