8420f8875733cc057a1f1c1de81d9543bb00c030
michellemccth3 <70456429+michellemccth3@users.noreply.github.com>
2020-10-22 00:58:35 -0400

    Update HousingApplication.java

--- HousingApplication.java
+++ HousingApplication.java
@@ -1,19 +1,28 @@
 package housingapplication;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.*;
+import java.util.ArrayList; 
 
-public class HousingApplication extends Application{
-   public void init(){
-   
-   }
-   
-   public void start(Stage stage){
-   
-   {
-   
-   public void stop(){
-   
-   }
+public class HousingApplication{
+	private ArrayList<Tenant> tenants; 
+	private ArrayList<Owner> owners;
+	private ArrayList<Property> properties;
+	
+	public HousingApplication() {
+		tenants = Database.getTenants();
+		owners = Database.getOwners();
+		properties = Database.getProperties();
+	}
+	
+	public Property searchProperties() {
+		
+		return null;
+	}
+	
+	public void rateProperty() {
+		
+	}
+	
+	public void signLease() {
+		
+	}
 }
