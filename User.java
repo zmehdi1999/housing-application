72910b880a1916be17b0fb024e5ef129422aef49
Douglas Wendel <70234982+dpwendel@users.noreply.github.com>
2020-10-22 14:06:42 -0400

    Update User.java

--- User.java
+++ User.java
@@ -12,6 +12,18 @@ public class User {
 	protected boolean owner;
 	protected int rating;
 	
+	public User(String firstName, String lastName, String userName, String email, String password, int phoneNum, bool registered, bool tenant, bool owner, int rating) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.userName = userName;
+		this.email = email;
+		this.password = password;
+		this.phoneNum = phoneNum;
+		this.registered = registered;
+		this.tenant = tenant;
+		this.owner = owner;
+		this.rating = rating; 
+	}
 	public User createAccount(User user)
 	{
 		return user;
