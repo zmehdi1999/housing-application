b40c641385934c8d48b9c42d8e5c27d1a947ac8c
jmccutcheon16 <49525814+jmccutcheon16@users.noreply.github.com>
2020-10-22 14:47:01 -0400

    Changes to constants

--- DataConstants.java
+++ DataConstants.java
@@ -2,14 +2,15 @@
 public abstract class DataConstants {
 
 	protected static final String FILE = "src/json_User.json";
-	protected static final String USER_FIRST_NAME = "";
-	protected static final String USER_LAST_NAME = "";
-	protected static final String USER_EMAIL = "";
-	protected static final String USER_PASSWORD = "";
-	protected static final int USER_PHONE_NUM = "";
-	protected static final boolean REGISTERED = "";
-	protected static final boolean TENANT = "";
-	protected static final boolean OWNER = "";
-	protected static final int RATING = "";
+	protected static final String USER_USER_NAME = "";
+	protected static final String USER_FIRST_NAME = "firstName";
+	protected static final String USER_LAST_NAME = "lastName";
+	protected static final String USER_EMAIL = "email";
+	protected static final String USER_PASSWORD = "password";
+	protected static final String REGISTERED = "registered";
+	protected static final String OWNER = "owner";
+	
+	protected static final int RATING = "rating";
+	protected static final int USER_PHONE_NUM = "phoneNumber";
 	
 }
