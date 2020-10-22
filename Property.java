65152b78e09528469f08f3900e99afe80e1f0448
Douglas Wendel <70234982+dpwendel@users.noreply.github.com>
2020-10-22 13:48:48 -0400

    Update Property.java

--- Property.java
+++ Property.java
@@ -1,3 +1,45 @@
 public class Property {
 	
+	public bool Vacancy() {
+		
+	}
+	
+	public String Location() {
+	}
+	public double Price() {
+	}
+	
+	public int yearBuilt() {
+	}
+	
+	public int numBed() {
+		
+	}
+	public int numBath() {
+	}
+	public bool washerAndDryer() {
+	}
+	
+	public bool pool() {
+	}
+	public bool parking() {
+	}
+	public bool lawncare() {
+	}
+	public int vistaWS() {
+	}
+	public int fpWS() {
+	}
+	public int uscWS() {
+	}
+	public ArrayList<String> userReviews() {
+	}
+	public ArrayList<String> landlordReviews() {
+		
+	}
+		
+		
+	
+	
+	
 }
