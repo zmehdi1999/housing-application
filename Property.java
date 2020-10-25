0c47726dac36e5ade61d6c24adbe0701af8e1243
jmccutcheon16 <49525814+jmccutcheon16@users.noreply.github.com>
2020-10-25 15:20:39 -0400

    Fixed merging issue

--- Property.java
+++ Property.java
@@ -1,54 +1,4 @@
-<<<<<<< HEAD
-public class Property {
-	
-	public Property() {
-		
-	}
-	
-	public bool Vacancy() {
-		
-	}
-	
-	public String Location() {
-	}
-	public double Price() {
-	}
-	
-	public int yearBuilt() {
-	}
-	
-	public int numBed() {
-		
-	}
-	public int numBath() {
-	}
-	public bool washerAndDryer() {
-	}
-	
-	public bool pool() {
-	}
-	public bool parking() {
-	}
-	public bool lawncare() {
-	}
-	public int vistaWS() {
-	}
-	public int fpWS() {
-	}
-	public int uscWS() {
-	}
-	public ArrayList<String> userReviews() {
-	}
-	public ArrayList<String> landlordReviews() {
-		
-	}
-		
-		
-	
-	
-	
-}
-=======
+
 public class Property {
 	
 	protected String location;
@@ -177,10 +127,5 @@ public class Property {
 	public ArrayList<String> landlordReviews() {
 		
 	}
-		
-		
-	
-	
 	
 }
->>>>>>> branch 'main' of https://github.com/zmehdi1999/housing-application.git
