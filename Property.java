daa312325225e17e2a5a692245111cb6a9f0e116
Douglas Wendel <70234982+dpwendel@users.noreply.github.com>
2020-10-25 14:15:20 -0400

    Update Property.java

--- Property.java
+++ Property.java
@@ -1,37 +1,65 @@
 public class Property {
 	
-	public bool Vacancy() {
-		
-	}
+	protected String location;
+	protected boolean Vacancy;
+	protected double Price;
+	protected int yearBuilt;
+	protected int numBed;
+	protected int numBath;
+	protected boolean washerAndDryer;
+	protected boolean pool;
+	protected boolean parking;
+	protected boolean lawncare;
+	protected int vistaWS;
+	protected int fpWS;
+	protected int uscWS;
 	
-	public String Location() {
+	public Property(String location, String Vacancy, double Price, int yearBuilt, int numBed, int numBath, String washerAndDryer, String pool, String parking, String lawncare, int vistaWS, int fpWS, int uscWS){
+		this.location = location;
+		this.Vacancy = Vacancy;
+		this.Price = Price;
+		this.yearBuilt = yearBuilt;
+		this.numBed = numBed;
+		this.numBath = numBath;
+		this.washerAndDryer = washerAndDryer;
+		this.pool = pool;
+		this.parking = parking;
+		this.lawncare = lawncare;
+		this.vistaWS = vistaWS;
+		this.fpWS = fpWS;
+		this.uscWS = uscWS;
 	}
-	public double Price() {
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+/////////////////	Getters	    ////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+	public String getLocation() {
+		return location;
 	}
+	public boolean getVacancy() {
+		return Vacancy;
+	}
+	public double getPrice() {
+		return Price;
+	}
+	public int getYearBuilt() {
+		return yearBuilt;
+	}
+	public int getNumBed() {
+		return numBed;
+	}
+	public int getNumBath() {
+		return numBath;
+	}
+	public boolean getWasherAndDryer() {
+		return washerAndDryer;
+	}
+	public boolean pool() {
+		ret
+
 	
-	public int yearBuilt() {
-	}
 	
-	public int numBed() {
-		
-	}
-	public int numBath() {
-	}
-	public bool washerAndDryer() {
-	}
-	
-	public bool pool() {
-	}
-	public bool parking() {
-	}
-	public bool lawncare() {
-	}
-	public int vistaWS() {
-	}
-	public int fpWS() {
-	}
-	public int uscWS() {
-	}
 	public ArrayList<String> userReviews() {
 	}
 	public ArrayList<String> landlordReviews() {
