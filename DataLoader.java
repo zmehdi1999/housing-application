182c60d2b66ef2e5b82d153864a5af30a473b791
jmccutcheon16 <49525814+jmccutcheon16@users.noreply.github.com>
2020-10-25 15:29:41 -0400

    DataLoader fixed syntax

--- DataLoader.java
+++ DataLoader.java
@@ -33,33 +33,45 @@ public class DataLoader {
 				users.add(new User(firstName, lastName, userName, email, password, phoneNum, registered, owner, rating));
 			}
 			return users;
-		}	catch(Exception e) {
+		}
+			catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
 	
 	public static ArrayList<Listings> loadListings() {
 		ArrayList<Listings> listings = new ArrayList<Listings>();
 		
 		try {
 			FileReader reader = new FileReader(PROPERTY_FILE);
-			String location = (String)json_Property.get(PROPERTY_LOCATION);
-			String vacancy = (String)json_Property.get(PROPERTY_VACANCY);
-			double price = (double)json_Property.get(PROPERTY_PRICE);
-			int yearBuilt = (int)json_Property.get(PROPERTY_YEARBUILT);
-			int numBed = (int)json_Property.get(PROPERTY_NUMBED);
-			int numBath = (int)json_Property.get(PROPERTY_NUMBATH);
-			String washerAndDryer = (String)json_Property.get(PROPERTY_WASHERANDDRYER);
-			String pool = (String)json_Property.get(PROPERTY_POOL);
-			String parking = (String)json_Property.get(PROPERTY_PARKING);
-			String lawncare = (String)json_Property.get(PROPERTY_LAWNCARE);
-			int vistaWS = (int)json_Property.get(PROPERTY_VISTAWS);
-			int fpWS = (int)json_Property.get(PROPERTY_FPWS);
-			int uscWS = (int)json_Property.get(PROPERTY_USCWS);
+			JSONParser parser = new JSONParser();
+			JSONArray usersJSON = (JSONArray)new JSONParser().parse(reader);
 			
+			
+			for(int i = 0; i < json_Property.size(); i++) {
+				String location = (String)json_Property.get(PROPERTY_LOCATION);
+				String vacancy = (String)json_Property.get(PROPERTY_VACANCY);
+				double price = (double)json_Property.get(PROPERTY_PRICE);
+				int yearBuilt = (int)json_Property.get(PROPERTY_YEARBUILT);
+				int numBed = (int)json_Property.get(PROPERTY_NUMBED);
+				int numBath = (int)json_Property.get(PROPERTY_NUMBATH);
+				String washerAndDryer = (String)json_Property.get(PROPERTY_WASHERANDDRYER);
+				String pool = (String)json_Property.get(PROPERTY_POOL);
+				String parking = (String)json_Property.get(PROPERTY_PARKING);
+				String lawncare = (String)json_Property.get(PROPERTY_LAWNCARE);
+				int vistaWS = (int)json_Property.get(PROPERTY_VISTAWS);
+				int fpWS = (int)json_Property.get(PROPERTY_FPWS);
+				int uscWS = (int)json_Property.get(PROPERTY_USCWS);
+			}
 			
 			listings.add(new Property(location, vacancy, price, yearBuild, numBed, numBath, washerAndDryer, pool, parking, lawncare, vistaWS, fpWS, uscWS));
-			}
+		}
+		
 		return listings;
+		
 		}	catch(Exception e) {
 			e.printStackTrace();
 		}
+		return null;
+	}
+}
