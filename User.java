9d2fbcad442df78e2685d72ce1a1e5c4f836be04
jmccutcheon16 <49525814+jmccutcheon16@users.noreply.github.com>
2020-10-27 13:30:57 -0400

    bool to boolean fix

--- User.java
+++ User.java
@@ -12,7 +12,7 @@ public class User {
 	protected boolean owner;
 	protected int rating;
 	
-	public User(String firstName, String lastName, String userName, String email, String password, int phoneNum, bool registered, bool tenant, bool owner, int rating) {
+	public User(String firstName, String lastName, String userName, String email, String password, int phoneNum, boolean registered, boolean tenant, boolean owner, int rating) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.userName = userName;
@@ -49,13 +49,13 @@ public class User {
 	public int getPhoneNum() {
 		return phoneNum;
 	}
-	public bool getRegistered() {
+	public boolean getRegistered() {
 		return registered;
 	}
-	public bool getTenant() {
+	public boolean getTenant() {
 		return tenant;
 	}
-	public bool getOwner() {
+	public boolean getOwner() {
 		return owner;
 	}
 	public int getRating() {
@@ -87,13 +87,13 @@ public class User {
 	public void setPhoneNum(int phoneNum) {
 		this.phoneNum = phoneNum;
 	}
-	public void setRegistered(bool registered){
+	public void setRegistered(boolean registered){
 		this.registered = registered;
 	}
-	public void setTenant(bool tenant){
+	public void setTenant(boolean tenant){
 		this.tenant = tenant;
 	}
-	public void setOwner(bool owner) {
+	public void setOwner(boolean owner) {
 		this.owner = owner;
 	}
 	public void setRating(int rating) {
