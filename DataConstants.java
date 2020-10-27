2108f7b89aecb941403bfed8536d865f0da16011
jmccutcheon16 <49525814+jmccutcheon16@users.noreply.github.com>
2020-10-27 14:01:21 -0400

    Push

--- DataConstants.java
+++ DataConstants.java
@@ -1,15 +1,15 @@
 
 public abstract class DataConstants {
 
-	protected static final String USER_FILE = "src/json_User.json";
+	protected static final String FILE = "src/json_User.json";
 	protected static final String PROPERTY_FILE = "src/json_Property.json";
 	protected static final String USER_USER_NAME = "";
 	protected static final String USER_FIRST_NAME = "firstName";
 	protected static final String USER_LAST_NAME = "lastName";
 	protected static final String USER_EMAIL = "email";
 	protected static final String USER_PASSWORD = "password";
-	protected static final String REGISTERED = "registered";
-	protected static final String OWNER = "owner";
+	protected static final String USER_REGISTERED = "registered";
+	protected static final String USER_OWNER = "owner";
 	
 	protected static final int USER_RATING = 0;
 	protected static final int USER_PHONE_NUM = 0;
--- DataLoader.java
+++ DataLoader.java
@@ -5,7 +5,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-public class DataLoader {
+public class DataLoader extends DataConstants{
 	
 	public static ArrayList<User> loadUsers(){
 	
@@ -17,18 +17,18 @@ public class DataLoader {
 			JSONArray usersJSON = (JSONArray)new JSONParser().parse(reader);
 			
 			
-			for(int i = 0; i < json_User.size(); i++) {
-				String firstName = (String)json_USER.get(USER_FIRST_NAME);
-				String lastName = (String)json_USER.get(USER_LAST_NAME);
-				String userName = (String)json_USER.get(USER_USER_NAME);
-				String email = (String)json_USER.get(USER_EMAIL);
-				String password = (String)json_USER.get(USER_PASSWORD);
-				int phoneNum = (int)json_USER.get(USER_PHONE_NUM);
-				String registered = (String)json_USER.get(USER_REGISTERED);
+			for(int i = 0; i < usersJSON.size(); i++) {
+				String firstName = (String)usersJSON.get(USER_FIRST_NAME);
+				String lastName = (String)usersJSON.get(USER_LAST_NAME);
+				String userName = (String)usersJSON.get(USER_USER_NAME);
+				String email = (String)usersJSON.get(USER_EMAIL);
+				String password = (String)usersJSON.get(USER_PASSWORD);
+				int phoneNum = (int)usersJSON.get(USER_PHONE_NUM);
+				String registered = (String)usersJSON.get(USER_REGISTERED);
 				//boolean tenant =(boolean)json_TenantAndOwner.get(USER_REGISTERED);
 				//we dont need tenant, bc if owner is false then they know they are a tenant, need to make OWNER a String not boolean
-				String owner = (String)json_USER.get(USER_OWNER);
-				int rating = (int) json_USER.get(USER_RATING);
+				String owner = (String)usersJSON.get(USER_OWNER);
+				int rating = (int)usersJSON.get(USER_RATING);
 				
 				users.add(new User(firstName, lastName, userName, email, password, phoneNum, registered, owner, rating));
 			}
