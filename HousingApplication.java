ff689615b0c2c192e3f31a97402d45e6fe04864e
michellemccth3 <70456429+michellemccth3@users.noreply.github.com>
2020-10-27 13:20:10 -0400

    Updated HousingApplication and HousingApplicationUI classes

--- HousingApplication.java
+++ HousingApplication.java
@@ -12,16 +12,37 @@ public class HousingApplication{
 		properties = Database.getProperties();
 	}
 	
-	public Property searchProperties() {
-		
-		return null;
-	}
-	
-	public void rateProperty() {
+	public void login(String username, String password) {
 		
 	}
 	
-	public void signLease() {
+	public ArrayList<Property> searchProperties(int minPrice, int maxPrice, int numBed, int numBath, boolean washerAndDryer, boolean pool, boolean lawnCare) {
+		ArrayList<Property> found = new ArrayList<Property>();
+		Property property;
+		for(int i = 0; i < properties.size(); i++) {
+			property = properties.get(i);
+			if(property.getPrice() >= minPrice && property.getPrice() <= maxPrice);
+			else continue;
+			if(property.getNumBed() == numBed);
+			else continue;
+			if(property.getNumBath() == numBath);
+			else continue;
+			if(property.getWasherAndDryer() != null && washerAndDryer == true);
+			else continue;
+			if(property.getPool() != null && pool == true);
+			else continue;
+			if(property.getLawncare() != null && lawnCare == true);
+			else continue;
+			found.add(property);
+		}
+		return found;
+	}
+	
+	public void reviewProperty(Property property) {
+		
+	}
+	
+	public void signLease(Property property) {
 		
 	}
 }
--- HousingApplicationUI.java
+++ HousingApplicationUI.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class HousingApplicationUI {
 	private Scanner scanner;
 	private HousingApplication application;
+	private String[] mainMenuOptions = {"Login","Search Properties","Review Property","Sign Lease","Close Application"};
 	
 	HousingApplicationUI(){
 		scanner = new Scanner(System.in);
@@ -11,17 +12,60 @@ public class HousingApplicationUI {
 	}
 	
 	private void run() {
-		
+		while(true) {
+			System.out.println();
+			displayMainMenu();
+			int userCommand = getUserCommand(mainMenuOptions.length);
+			
+			if(userCommand == mainMenuOptions.length -1) break;
+			
+			switch(userCommand) {
+			case(0):
+				login();
+				break;
+			case(1):
+				searchProperties();
+				break;
+			case(2):
+				reviewProperty();
+				break;
+			case(3):
+				signLease();
+				break;
+		}
+		}
 	}
 	
+	private void displayMainMenu() {
+		System.out.println("\nMain Menu:");
+		for(int i=0; i< mainMenuOptions.length; i++) {
+			System.out.println((i+1) + ". " + mainMenuOptions[i]);
+		}
+		System.out.println("\n");
+	}
 	private int getUserCommand(int numCommands) {
-		
+		System.out.println("\nWhat option would you like to pick?: ");
+		String input = scanner.nextLine();
+		int command = Integer.parseInt(input) - 1;
+		if(command >= 0 && command <= numCommands -1) return command;
 		return -1;
+	}
+	
+	private void login() {
+		
 	}
 	
 	private boolean searchProperties() {
 		
 		return true;
+	}
+	
+	private void reviewProperty() {
+		
+	}
+	
+	private void signLease() {
+		
 	}
 	
 	public static void main(String[] args) {
