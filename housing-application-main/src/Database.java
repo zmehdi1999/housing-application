461dd36e62c88c7d8eb8e660b46a63964a1f3f5d
zmehdi1999 <69934544+zmehdi1999@users.noreply.github.com>
2020-10-29 19:02:31 -0400

    Add files via upload

---
+++ housing-application-main/src/Database.java
@@ -0,0 +1,16 @@
+
+import java.util.ArrayList;
+
+public class Database {
+	public static ArrayList<Tenant> getTenants(){
+		return new ArrayList<Tenant>();
+	}
+	
+	public static ArrayList<Owner> getOwners(){
+		return new ArrayList<Owner>();
+	}
+	
+	public static ArrayList<Property> getProperties(){
+		return new ArrayList<Property>();
+	}
+}
